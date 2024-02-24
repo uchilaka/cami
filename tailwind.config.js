f852dd9ac18c82ef8be0ea040f8496f23d2d5c4e
Uche Chilaka <uche@lar.city>
2024-02-24 04:40:12 -0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/views/**/*.{html,html.erb,erb}',
+    './app/views/devise/**/*.{html,html.erb,erb}',
+    './app/frontend/components/**/*.{vue,js,ts,jsx,tsx}',
+    './app/frontend/**/*.{vue,js,ts,jsx,tsx}',
+    './app/**/*.{vue,js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms')
+  ],
+}
