a132043fe0107cd147a1e8006a0347fe8734f2b9
Uche Chilaka <uche@lar.city>
2024-02-26 01:48:28 -0500

    SAVEPOINT

--- spec/models/individual_spec.rb
+++ spec/models/individual_spec.rb
@@ -1,5 +1,17 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: accounts
+#
+#  id           :uuid             not null, primary key
+#  display_name :string           not null
+#  readme       :text
+#  type         :string           not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  tax_id       :string
+#
 require 'rails_helper'
 
 RSpec.describe Individual, type: :model do
