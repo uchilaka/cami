55596a4c5d0c4acf6edd0c3a0aaefb3bd8c9fe36
Uche Chilaka <uche@lar.city>
2024-03-13 05:23:16 -0400

    refactor: some cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,6 @@ const { content, theme, plugins } = require('./config/tailwind.config')
 module.exports = {
   content: [
     ...content,
-    // './app/views/**/*.{html,html.erb,erb}',
-    // './app/views/devise/**/*.{html,html.erb,erb}',
     './app/frontend/components/**/*.{vue,js,ts,jsx,tsx}',
     './app/frontend/**/*.{vue,js,ts,jsx,tsx}',
     './app/**/*.{vue,js,ts,jsx,tsx}',
