a35e42ec7334ed67f642dd0e7c02993d306c76b5
Uche Chilaka <uche@lar.city>
2024-03-13 10:02:05 -0400

    chore: bundle & migrate updates

--- db/schema.rb
+++ db/schema.rb
@@ -70,6 +70,20 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_25_091819) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "products_services", id: false, force: :cascade do |t|
+    t.uuid "service_id", null: false
+    t.uuid "product_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "services", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "display_name", null: false
+    t.text "readme"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -9,6 +9,7 @@
 #  description  :text
 #  display_name :string           not null
 #  sku          :string
+#  type         :string           not null
 #  created_at   :datetime         not null
 #  updated_at   :datetime         not null
 #  vendor_id    :uuid
