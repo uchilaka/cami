fbc67176afbc6d4c20df30203ed7a2a7962cf0d6
Uche Chilaka <uche@lar.city>
2024-04-04 13:35:32 -0400

    refactor: several app updates for config, logo & views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
   def omniauth_authorize_path(_resource_name, _provider)
     user_google_omniauth_authorize_path
   end
+
+  def application_title
+    Rails.application.class.module_parent_name
+  end
 end
