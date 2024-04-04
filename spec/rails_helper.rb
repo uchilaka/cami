51f4b95548b3af5e74d2341ab418899c904ede0f
Uche Chilaka <uche@lar.city>
2024-04-04 15:38:28 -0400

    refactor: a few more setup updates

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,14 +4,15 @@
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-require 'database_cleaner/active_record'
-require 'sidekiq/testing'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'rspec/rails'
+require 'database_cleaner/active_record'
+require 'sidekiq/testing'
+require 'devise/test/integration_helpers'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -35,7 +36,10 @@ begin
 rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
+
 RSpec.configure do |config|
+  config.fail_fast = true
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = Rails.root.join('spec/fixtures')
 
@@ -66,6 +70,9 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # Devise integration helpers https://github.com/heartcombo/devise?tab=readme-ov-file#integration-tests
+  config.include Devise::Test::IntegrationHelpers, type: :feature
 
   config.before(:suite) do
     # Database cleaner setup: https://github.com/DatabaseCleaner/database_cleaner?tab=readme-ov-file#rspec-example
