0bbb7d172edd971c64ecf381534a3c5fe687273d
Uche Chilaka <uche@lar.city>
2024-04-09 23:58:39 -0400

    chore: SAVEPOINT

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 module AccountsHelper
+  def account_summary_template
+    return 'account-summary-card' if Flipper.enabled?(:feat__account_summary_card, current_user)
+
+    'account-summary'
+  end
 end
