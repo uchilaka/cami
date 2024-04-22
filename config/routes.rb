08f776b6fe03d187888a29d139567297279eb2ed
Uche Chilaka <uche@lar.city>
2024-04-22 05:02:47 -0400

    refactor: some cleanup & tweaks

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   devise_for :users,
              controllers: {
                sessions: 'users/passwordless',
+               passwords: 'users/passwords',
                registrations: 'users/registrations',
                confirmations: 'users/confirmations',
                unlocks: 'users/unlocks',
