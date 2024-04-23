25125b230f66cee3458110b8648f5d0c2df9eb35
Uche Chilaka <uche@lar.city>
2024-04-23 06:29:24 -0400

    refactor: got the implementation working via TDD

--- app/helpers/pii_helper.rb
+++ app/helpers/pii_helper.rb
@@ -13,41 +13,40 @@ module PIIHelper
     sanitize_hash(parsed_data)
   end
 
-  def sanitize_hash(hash)
-    hash.each do |key, value|
+  def sanitize_hash(hash, debug: false)
+    _old_hash = hash.deep_dup if debug
+    hash.each do |label, value|
       _old_value = value
       case value
       when Hash
         sanitize_hash(value)
       when String
-        hash[key] = replace_pii(key, value)
+        hash[label] = replace_pii(value, label:)
       when Array
-        hash[key] = value.map { |e| sanitize_hash(e) }
+        hash[label] = value.map { |e| sanitize_hash(e) }
       end
     end
     # Return the sanitized hash
     hash
   end
 
-  def replace_pii(label, default_value = nil)
+  def replace_pii(default_value = nil, label:)
     # Replace PII patterns with corresponding fake data generation
-    label.to_s.gsub(/name|email|phone|address|social_security_number|credit_card_number/i) do |match|
-      case match
-      when /(sur)?name/i, /(business|given|family|full)_name/i
-        Faker::Name.neutral_first_name
-      when /email/i
-        Faker::Internet.email
-      when /phone/i
-        Faker::PhoneNumber.phone_number
-      when /address/i
-        Faker::Address.full_address
-      when /social_security_number/i
-        '***-**-****'
-      when /credit_card_number/i
-        '****-****-****-****'
-      else
-        default_value
-      end
+    case label
+    when /(sur)?name/i, /(business|given|family|full)_name/i
+      Faker::Name.neutral_first_name
+    when /email/i
+      Faker::Internet.email
+    when /phone/i
+      Faker::PhoneNumber.phone_number
+    when /address/i
+      Faker::Address.full_address
+    when /social_security_number/i
+      '***-**-****'
+    when /credit_card_number/i
+      '****-****-****-****'
+    else
+      default_value
     end
   end
 end
--- spec/helpers/pii_helper_spec.rb
+++ spec/helpers/pii_helper_spec.rb
@@ -7,9 +7,9 @@ RSpec.describe PIIHelper do
 
   describe '#replace_pii' do
     let(:value) { 'John Doe' }
-    let(:key) { 'given_name' }
+    let(:label) { 'given_name' }
 
-    subject { mock_class.replace_pii(key, value) }
+    subject { mock_class.replace_pii(value, label:) }
 
     context 'with given_name' do
       it 'replaces the given name with a fake name' do
@@ -19,10 +19,47 @@ RSpec.describe PIIHelper do
 
     context 'with name' do
       let(:value) { 'John Doe' }
-      let(:key) { 'name' }
+      let(:label) { 'name' }
 
       it 'replaces the name with a fake name' do
         expect(subject).not_to eq(value)
+      end
+    end
+
+    context 'with email' do
+      let(:value) { 'tom.jefferson@hey.com' }
+      let(:label) { 'email' }
+
+      it 'replaces the email with a fake email' do
+        expect(subject).not_to eq(value)
+      end
+    end
+
+    context 'with phone' do
+      let(:value) { '555-444-1234' }
+      let(:label) { 'phone' }
+
+      it 'replaces the phone with a fake phone number' do
+        expect(subject).not_to eq(value)
+      end
+    end
+
+    context 'when value is nil' do
+      let(:value) { nil }
+      let(:label) { 'phone' }
+
+      it 'generates a fake phone number' do
+        expect(subject).not_to eq(value)
+      end
+    end
+
+    context 'when a default value is not provided' do
+      let(:label) { 'given_name' }
+
+      subject { mock_class.replace_pii(label:) }
+
+      it 'generates a fake name' do
+        expect(subject).not_to be_nil
       end
     end
   end
