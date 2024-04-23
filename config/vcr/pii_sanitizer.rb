1345b3608e5f4e2fc83841f5575849c97113a06b
Uchenna Chilaka <2763396+uchilaka@users.noreply.github.com>
2024-04-23 08:07:09 -0400

    refactor: ChatGPT comment left in here LOL

--- config/vcr/pii_sanitizer.rb
+++ config/vcr/pii_sanitizer.rb
@@ -6,7 +6,6 @@ module PIISanitizer
   extend PIIHelper
 
   def self.sanitize(data)
-    # Include the `sanitize_json` function from previous example
     JSON.generate(sanitize_json(data))
   end
 end
