6532d69d96db2c883a4e60d90ab9629f27d950af
Uche Chilaka <uche@lar.city>
2024-09-14 05:34:07 -0400

    fix: spec invoice fabrication (some rubocopping too)

--- spec/views/invoices/index.html.tailwindcss_spec.rb
+++ spec/views/invoices/index.html.tailwindcss_spec.rb
@@ -1,14 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe "invoices/index", type: :view do
+RSpec.describe 'invoices/index', type: :view do
   before(:each) do
-    assign(:invoices, [
-      Invoice.create!(),
-      Invoice.create!()
-    ])
+    assign(:invoices, [Fabricate(:invoice), Fabricate(:invoice)])
   end
 
-  it "renders a list of invoices" do
+  it 'renders a list of invoices' do
     render
     cell_selector = Rails::VERSION::STRING >= '7' ? 'div>p' : 'tr>td'
   end
--- spec/views/invoices/show.html.tailwindcss_spec.rb
+++ spec/views/invoices/show.html.tailwindcss_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe "invoices/show", type: :view do
+RSpec.describe 'invoices/show', type: :view do
   before(:each) do
-    assign(:invoice, Invoice.create!())
+    assign(:invoice, Fabricate(:invoice))
   end
 
-  it "renders attributes in <p>" do
+  it 'renders attributes in <p>' do
     render
   end
 end
