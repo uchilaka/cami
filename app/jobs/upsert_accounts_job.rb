3bef6dd9bd6365aac375c7a686ec3e0643c18d16
Uche Chilaka <uche@lar.city>
2024-09-14 09:01:19 -0400

    chore: SAVEPOINT

--- app/jobs/upsert_accounts_job.rb
+++ app/jobs/upsert_accounts_job.rb
@@ -11,6 +11,17 @@ class UpsertAccountsJob < ApplicationJob
       .where.not(status: %w[CANCELED REFUNDED])
       .each do |invoice|
       Rails.logger.info "Found accounts for #{invoice.id}", accounts: invoice.accounts
+      invoice.accounts.each do |account|
+        if (accounts = Account.where(email: account['email'], type: account['type']))
+          Rails.logger.warn("Found matching account(s) for #{account['email']}", accounts:)
+        else
+          new_account = Account.new(account)
+          new_account
+        end
+
+        Rails.logger.info "Upserting account #{account['id']} for invoice #{invoice.id}"
+        upsert_account(account)
+      end
     end
   end
 end
