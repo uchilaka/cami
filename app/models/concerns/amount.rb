6e7099576ed58db405fd2e97980e357abca52e36
Uche Chilaka <uche@lar.city>
2024-09-27 20:25:40 -0400

    refactor: got Amount class working

--- app/models/concerns/amount.rb
+++ app/models/concerns/amount.rb
@@ -4,17 +4,28 @@ class Amount
   include ActiveModel::Model
   include ActiveSupport::NumberHelper
 
-  attr_accessor :value, :currency_code
+  NON_DECIMAL_DELIMITER_REGEX = /[,\s]/
+
+  attr_accessor :value, :currency_code, :error, :error_value
 
   def initialize(attributes = {})
     attributes ||= {}
-    @currency_code = attributes[:currency_code] || 'USD'
-    # TODO: Parse into a decimal string ready for calculations
-    @value = attributes[:value]
+    new_currency_code, new_value = attributes.values_at :currency_code, :value
+    @currency_code = new_currency_code || 'USD'
+    @value =
+      if new_value.is_a?(Numeric)
+        new_value
+      elsif new_value.is_a?(String)
+        new_value.gsub(NON_DECIMAL_DELIMITER_REGEX, '').to_f
+      else
+        @error = ArgumentError.new('Not a string or number')
+        @error_value = new_value
+        0.0
+      end
   end
 
   def to_h
-    { value:, currency_code: }
+    { value:, currency_code:, error:, error_value: }.compact
   end
 
   def self.from_json(json)
