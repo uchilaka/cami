77c110caba192e1eafd71fb568987698e67d5bc7
Uche Chilaka <uche@lar.city>
2024-10-03 05:30:15 -0400

    refactor: (WIP) loading animation

--- app/frontend/components/AccountSummaryModal.tsx
+++ app/frontend/components/AccountSummaryModal.tsx
@@ -4,8 +4,10 @@ import { nsEventName, LoadAccountEventDetail } from '@/utils'
 import withAllTheProviders from '@/components/withAllTheProviders'
 import { getAccount } from '@/utils/api'
 import { BusinessAccount, IndividualAccount } from '@/utils/api/types'
+import LoadingAnimation from './LoadingAnimation'
 
 interface AccountContextProps {
+  loading?: boolean
   account?: IndividualAccount | BusinessAccount | null
   reload: () => Promise<void>
   listenForAccountLoadEvents: () => AbortController
@@ -40,6 +42,8 @@ const AccountProvider = ({ children }: { children: React.ReactNode }) => {
   const { account, query } = useAccountSummaryQuery(accountId)
 
   const reload = useCallback(async () => {
+    console.debug(`Loading account: ${accountId}`)
+    setLoading(true)
     const result = await query.refetch()
     console.debug({ result })
     if (result.isSuccess) setLoading(false)
@@ -65,17 +69,13 @@ const AccountProvider = ({ children }: { children: React.ReactNode }) => {
   }
 
   useEffect(() => {
-    if (loading === undefined) setLoading(true)
-    if (accountId) {
-      console.debug(`Loading account: ${accountId}`)
-      reload()
-    }
+    if (accountId) reload()
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [accountId])
 
   console.debug({ account })
 
-  return <AccountContext.Provider value={{ account, reload, listenForAccountLoadEvents }}>{children}</AccountContext.Provider>
+  return <AccountContext.Provider value={{ loading, account, reload, listenForAccountLoadEvents }}>{children}</AccountContext.Provider>
 }
 
 const withAccountProvider = <P extends {}>(WrappedComponent: ComponentType<P>) => {
@@ -101,7 +101,7 @@ const AccountSummaryModal: React.FC<ComponentProps<'div'>> = ({ children, id, ..
 
   const modalId = id || 'account--summary-modal'
 
-  const { account, listenForAccountLoadEvents } = useAccountContext()
+  const { loading, account, listenForAccountLoadEvents } = useAccountContext()
 
   useEffect(() => {
     if (!accountLoader) setAccountLoader(listenForAccountLoadEvents())
@@ -149,22 +149,28 @@ const AccountSummaryModal: React.FC<ComponentProps<'div'>> = ({ children, id, ..
           </div>
           {/* Modal Body */}
           <div className="p-4 md:p-5 space-y-4">
-            <p className="my-5">
-              You can find me in: <span className="px-2 py-1 bg-slate-100">frontend/components/AccountSummaryModal.tsx</span>
-            </p>
-            {/* Modal Actions */}
-            <hr className="my-4" />
-            <div className="flex justify-end items-center space-x-4">
-              <button className="btn px-5 py-2.5 text-base font-medium text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm text-center me-2 mb-2">
-                Edit this account
-              </button>
-              <button className="btn px-5 py-2.5 hover:text-white border focus:ring-4 focus:outline-none font-medium rounded-lg text-sm text-center me-2 mb-2 dark:hover:text-white text-red-700 border-red-700 hover:bg-red-800 focus:ring-red-300 dark:border-red-500 dark:text-red-500 dark:hover:bg-red-600 dark:focus:ring-red-900">
-                Delete this account
-              </button>
-              <button className="btn px-5 py-2.5 hover:text-white border focus:ring-4 focus:outline-none font-medium rounded-lg text-sm text-center me-2 mb-2 dark:hover:text-white text-gray-800 border-gray-800 hover:bg-gray-900 focus:ring-gray-300 dark:border-gray-600 dark:text-gray-400 dark:hover:bg-gray-600 dark:focus:ring-gray-800">
-                Back to accounts
-              </button>
-            </div>
+            {loading ? (
+              <LoadingAnimation />
+            ) : (
+              <>
+                <p className="my-5">
+                  You can find me in: <span className="px-2 py-1 bg-slate-100">frontend/components/AccountSummaryModal.tsx</span>
+                </p>
+                {/* Modal Actions */}
+                <hr className="my-4" />
+                <div className="flex justify-end items-center space-x-4">
+                  <button className="btn px-5 py-2.5 text-base font-medium text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm text-center me-2 mb-2">
+                    Edit this account
+                  </button>
+                  <button className="btn px-5 py-2.5 hover:text-white border focus:ring-4 focus:outline-none font-medium rounded-lg text-sm text-center me-2 mb-2 dark:hover:text-white text-red-700 border-red-700 hover:bg-red-800 focus:ring-red-300 dark:border-red-500 dark:text-red-500 dark:hover:bg-red-600 dark:focus:ring-red-900">
+                    Delete this account
+                  </button>
+                  <button className="btn px-5 py-2.5 hover:text-white border focus:ring-4 focus:outline-none font-medium rounded-lg text-sm text-center me-2 mb-2 dark:hover:text-white text-gray-800 border-gray-800 hover:bg-gray-900 focus:ring-gray-300 dark:border-gray-600 dark:text-gray-400 dark:hover:bg-gray-600 dark:focus:ring-gray-800">
+                    Back to accounts
+                  </button>
+                </div>
+              </>
+            )}
           </div>
         </div>
       </div>
