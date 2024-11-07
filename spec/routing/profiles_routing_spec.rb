86f607b378b635a6cc8f2d9ab4d30aa966156196
Uche Chilaka <uche@lar.city>
2024-11-06 23:39:05 -0500

    refactor: update /account/:account_id/profiles/* routing specs

--- spec/routing/profiles_routing_spec.rb
+++ spec/routing/profiles_routing_spec.rb
@@ -5,35 +5,35 @@ require 'rails_helper'
 RSpec.describe ProfilesController, type: :routing do
   describe 'routing' do
     it 'routes to #index' do
-      expect(get: '/metadata_profiles').to route_to('metadata_profiles#index')
+      expect(get: '/accounts/1/profiles').to route_to('profiles#index', account_id: '1')
     end
 
     it 'routes to #new' do
-      expect(get: '/metadata_profiles/new').to route_to('metadata_profiles#new')
+      expect(get: '/accounts/1/profiles/new').to route_to('profiles#new', account_id: '1')
     end
 
     it 'routes to #show' do
-      expect(get: '/metadata_profiles/1').to route_to('metadata_profiles#show', id: '1')
+      expect(get: '/accounts/1/profiles/1').to route_to('profiles#show', account_id: '1', id: '1')
     end
 
     it 'routes to #edit' do
-      expect(get: '/metadata_profiles/1/edit').to route_to('metadata_profiles#edit', id: '1')
+      expect(get: '/accounts/1/profiles/1/edit').to route_to('profiles#edit', account_id: '1', id: '1')
     end
 
     it 'routes to #create' do
-      expect(post: '/metadata_profiles').to route_to('metadata_profiles#create')
+      expect(post: '/accounts/1/profiles').to route_to('profiles#create', account_id: '1')
     end
 
     it 'routes to #update via PUT' do
-      expect(put: '/metadata_profiles/1').to route_to('metadata_profiles#update', id: '1')
+      expect(put: '/accounts/1/profiles/1').to route_to('profiles#update', account_id: '1', id: '1')
     end
 
     it 'routes to #update via PATCH' do
-      expect(patch: '/metadata_profiles/1').to route_to('metadata_profiles#update', id: '1')
+      expect(patch: '/accounts/1/profiles/1').to route_to('profiles#update', account_id: '1', id: '1')
     end
 
     it 'routes to #destroy' do
-      expect(delete: '/metadata_profiles/1').to route_to('metadata_profiles#destroy', id: '1')
+      expect(delete: '/accounts/1/profiles/1').to route_to('profiles#destroy', account_id: '1', id: '1')
     end
   end
 end
