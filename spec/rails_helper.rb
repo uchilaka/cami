6696cdaa03fc3547fb5dcd3e00a80ee1aae525f4
Uche Chilaka <uche@lar.city>
2024-11-10 02:22:19 -0500

    refactor: couple tweaks... not sure why CI was failing 😕

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -78,8 +78,6 @@ RSpec.configure do |config|
   # config.wait_delay = 1 # seconds
   # config.clone_wait_matcher = true
 
-  config.include Mongoid::Matchers, type: :model
-
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = Rails.root.join('spec/fixtures')
 
