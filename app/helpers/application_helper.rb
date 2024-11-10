fd157a6709fc91f8f01c22090d8907c6be5811a2
Uche Chilaka <uche@lar.city>
2024-11-10 02:13:39 -0500

    chore: SAVEPOINT

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,28 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  include StyleHelper
+
+  def modal_dom_id(resource, content_type: nil)
+    return "#{resource.model_name.singular}--#{content_type}--modal|#{resource.id}|" if content_type.present?
+
+    "#{resource.model_name.singular}-modal|#{resource.id}|"
+  end
+
+  def record_dom_id(resource, prefix: '')
+    dom_id = "#{resource.model_name.singular}-row-#{resource.id}"
+    if prefix.present?
+      "#{prefix}--#{dom_id}"
+    else
+      dom_id
+    end
+  end
+
+  def omniauth_authorize_path(_resource_name, _provider)
+    user_google_omniauth_authorize_path
+  end
+
+  def page_title
+    Rails.application.class.module_parent_name
+  end
 end
