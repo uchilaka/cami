18a78f355ba124b737d29b09303c81790e080bfb
Uche Chilaka <uche@lar.city>
2024-11-10 09:51:49 -0500

    refactor: setup dartass:watch script

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,12 @@ Rails.application.configure do
   # Enable web console for all IP addresses (see https://stackoverflow.com/a/71292229)
   # To enable all IP v6 addresses, use '::/0'
   config.web_console.permissions = '0.0.0.0/0'
+  # When a console cannot be shown for a given IP address or content type, messages
+  # such as the following is printed in the server logs:
+  # > Cannot render console from 192.168.1.133! Allowed networks: 127.0.0.0/127.255.255.255, ::1
+  #
+  # If you don't want to see this message anymore, set this option to false:
+  config.web_console.whiny_requests = true
 
   # Note on issues with calling the web console:
   # https://github.com/rails/web-console?tab=readme-ov-file#why-does-the-console-only-appear-on-error-pages-but-not-when-i-call-it
