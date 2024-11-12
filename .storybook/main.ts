2c384aa8b8c48e6ba09edac2cca2beddd0aea36a
Uche Chilaka <uche@lar.city>
2024-11-12 04:47:44 -0500

    refactor: pretty cool that SB can generate new stories right in the web app 😮😍

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,13 @@ import type { StorybookConfig } from '@storybook/react-vite'
 const config: StorybookConfig = {
   staticDirs: ['../public'],
   stories: ['../app/frontend/**/*.stories.@(js|jsx|ts|tsx)', '../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@storybook/addon-onboarding', '@storybook/addon-essentials', '@chromatic-com/storybook', '@storybook/addon-interactions'],
+  addons: [
+    '@storybook/addon-onboarding',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
+  ],
   core: {
     builder: '@storybook/builder-vite',
   },
