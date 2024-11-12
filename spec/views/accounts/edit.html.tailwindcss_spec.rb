4b0afcccda8c14198555cffa0344ed9738662ac1
Uche Chilaka <uche@lar.city>
2024-11-11 20:33:26 -0500

    chore: SAVEPOINT

--- spec/views/accounts/edit.html.tailwindcss_spec.rb
+++ spec/views/accounts/edit.html.tailwindcss_spec.rb
@@ -1,37 +1,29 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "accounts/edit", type: :view do
-  let(:account) {
-    Account.create!(
-      display_name: "MyString",
-      slug: "MyString",
-      status: 1,
-      type: "",
-      tax_id: "MyString",
-      readme: "MyText"
-    )
-  }
+RSpec.describe 'accounts/edit', type: :view do
+  let(:account) { Fabricate :account }
 
   before(:each) do
     assign(:account, account)
   end
 
-  it "renders the edit account form" do
+  it 'renders the edit account form' do
     render
 
-    assert_select "form[action=?][method=?]", account_path(account), "post" do
+    assert_select 'form[action=?][method=?]', account_path(account), 'post' do
+      assert_select 'input[name=?]', 'account[display_name]'
 
-      assert_select "input[name=?]", "account[display_name]"
+      assert_select 'input[name=?]', 'account[slug]'
 
-      assert_select "input[name=?]", "account[slug]"
+      assert_select 'input[name=?]', 'account[status]'
 
-      assert_select "input[name=?]", "account[status]"
+      assert_select 'input[name=?]', 'account[tax_id]'
 
-      assert_select "input[name=?]", "account[type]"
+      assert_select 'trix-editor#account_readme'
 
-      assert_select "input[name=?]", "account[tax_id]"
-
-      assert_select "textarea[name=?]", "account[readme]"
+      assert_select 'input[type="hidden"][name=?]', 'account[readme]'
     end
   end
 end
--- spec/views/accounts/index.html.tailwindcss_spec.rb
+++ spec/views/accounts/index.html.tailwindcss_spec.rb
@@ -1,35 +1,40 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "accounts/index", type: :view do
+RSpec.describe 'accounts/index', type: :view do
   before(:each) do
-    assign(:accounts, [
-      Account.create!(
-        display_name: "Display Name",
-        slug: "Slug",
-        status: 2,
-        type: "Type",
-        tax_id: "Tax",
-        readme: "MyText"
-      ),
-      Account.create!(
-        display_name: "Display Name",
-        slug: "Slug",
-        status: 2,
-        type: "Type",
-        tax_id: "Tax",
-        readme: "MyText"
-      )
-    ])
+    assign(
+      :accounts,
+      [
+        Fabricate(
+          :account,
+          display_name: 'Display Name',
+          slug: 'slug-1',
+          status: 'demo',
+          tax_id: '01-23456789',
+          readme: 'MyText'
+        ),
+        Fabricate(
+          :account,
+          display_name: 'Display Name',
+          slug: 'slug-2',
+          status: 'guest',
+          tax_id: '09-87654321',
+          readme: 'MyText'
+        )
+      ]
+    )
   end
 
-  it "renders a list of accounts" do
+  it 'renders a list of accounts' do
     render
     cell_selector = 'div>p'
-    assert_select cell_selector, text: Regexp.new("Display Name".to_s), count: 2
-    assert_select cell_selector, text: Regexp.new("Slug".to_s), count: 2
+    assert_select cell_selector, text: Regexp.new('Display Name'.to_s), count: 2
+    assert_select cell_selector, text: Regexp.new('slug-1'.to_s), count: 1
+    assert_select cell_selector, text: Regexp.new('slug-2'.to_s), count: 1
     assert_select cell_selector, text: Regexp.new(2.to_s), count: 2
-    assert_select cell_selector, text: Regexp.new("Type".to_s), count: 2
-    assert_select cell_selector, text: Regexp.new("Tax".to_s), count: 2
-    assert_select cell_selector, text: Regexp.new("MyText".to_s), count: 2
+    assert_select cell_selector, text: Regexp.new('Tax'.to_s), count: 2
+    assert_select cell_selector, text: Regexp.new('MyText'.to_s), count: 2
   end
 end
