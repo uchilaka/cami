18f1f744b475633b1dbb81b4b23a75af33581b44
Uche Chilaka <uche@lar.city>
2024-11-12 04:42:44 -0500

    chore: SAVEPOINT

---
+++ .storybook/main.ts
@@ -0,0 +1,15 @@
+import type { StorybookConfig } from '@storybook/react-vite'
+
+const config: StorybookConfig = {
+  staticDirs: ['../public'],
+  stories: ['../app/frontend/**/*.stories.@(js|jsx|ts|tsx)', '../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: ['@storybook/addon-onboarding', '@storybook/addon-essentials', '@chromatic-com/storybook', '@storybook/addon-interactions'],
+  core: {
+    builder: '@storybook/builder-vite',
+  },
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+}
+export default config
---
+++ .storybook/preview.ts
@@ -0,0 +1,47 @@
+import type { Preview } from '@storybook/react'
+import { initialize, mswLoader } from 'msw-storybook-addon'
+import { http, HttpResponse, delay } from 'msw'
+import mockFeaturesResponse from '../spec/fixtures/msw/feature_flags.json'
+import mockIndividualAccountResponse from '../spec/fixtures/msw/accounts/individual.json'
+import mockBusinessAccountResponse from '../spec/fixtures/msw/accounts/business.json'
+
+/*
+ * Initializes MSW
+ * See https://github.com/mswjs/msw-storybook-addon#configuring-msw
+ * to learn how to customize it
+ */
+initialize()
+
+import 'tailwindcss/tailwind.css'
+import 'flowbite/dist/flowbite.css'
+
+const preview: Preview = {
+  tags: ['autodocs'],
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    msw: {
+      handlers: [
+        http.get('/api/features?format=json', async () => {
+          await delay(150)
+          return HttpResponse.json(mockFeaturesResponse)
+        }),
+        http.get('/accounts/1234?format=json', async () => {
+          await delay(150)
+          return HttpResponse.json(mockIndividualAccountResponse)
+        }),
+        http.get('/accounts/4321?format=json', async () => {
+          await delay(150)
+          return HttpResponse.json(mockBusinessAccountResponse)
+        }),
+      ],
+    },
+  },
+  loaders: [mswLoader],
+}
+
+export default preview
