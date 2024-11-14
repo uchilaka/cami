e981a71d2d2b0bd0718741da6f86a1703890c1d8
Uche Chilaka <uche@lar.city>
2024-11-14 10:02:30 -0500

    chore: SAVEPOINT

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,20 +3,6 @@
 require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
-  # Note on issues with calling the web console:
-  # https://github.com/rails/web-console?tab=readme-ov-file#why-does-the-console-only-appear-on-error-pages-but-not-when-i-call-it
-  config.middleware.insert(0, Rack::Deflater)
-
-  # Enable web console for all IP addresses (see https://stackoverflow.com/a/71292229)
-  # To enable all IP v6 addresses, use '::/0'
-  config.web_console.permissions = '0.0.0.0/0'
-  # When a console cannot be shown for a given IP address or content type, messages
-  # such as the following is printed in the server logs:
-  # > Cannot render console from 192.168.1.133! Allowed networks: 127.0.0.0/127.255.255.255, ::1
-  #
-  # If you don't want to see this message anymore, set this option to false:
-  config.web_console.whiny_requests = true
-
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
