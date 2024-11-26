000f496a3a6fd77bc1709744716c47c77ba4df29
Uche Chilaka <uche@lar.city>
2024-11-26 09:49:28 -0500

    fix: (WIP) invoice edit spec

--- spec/views/invoices/edit.html.tailwindcss_spec.rb
+++ spec/views/invoices/edit.html.tailwindcss_spec.rb
@@ -3,22 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe 'invoices/edit', type: :view do
-  let(:invoice) do
-    Invoice.create!(
-      invoiceable_id: '',
-      invoiceable_type: 'MyString',
-      account: nil,
-      user: nil,
-      payments: '',
-      links: '',
-      invoice_number: 'MyString',
-      status: 1,
-      amount: '9.99',
-      due_amount: '9.99',
-      currency_code: 'MyString',
-      notes: 'MyText'
-    )
-  end
+  let(:invoiceable) { Fabricate :account }
+  let(:invoice) { Fabricate(:invoice, invoiceable:, amount: '9.99', due_amount: '9.99') }
 
   before(:each) do
     assign(:invoice, invoice)
@@ -28,17 +14,17 @@ RSpec.describe 'invoices/edit', type: :view do
     render
 
     assert_select 'form[action=?][method=?]', invoice_path(invoice), 'post' do
-      assert_select 'input[name=?]', 'invoice[invoiceable_id]'
-
-      assert_select 'input[name=?]', 'invoice[invoiceable_type]'
-
-      assert_select 'input[name=?]', 'invoice[account_id]'
-
-      assert_select 'input[name=?]', 'invoice[user_id]'
-
-      assert_select 'input[name=?]', 'invoice[payments]'
-
-      assert_select 'input[name=?]', 'invoice[links]'
+      # assert_select 'input[name=?]', 'invoice[invoiceable_id]'
+      #
+      # assert_select 'input[name=?]', 'invoice[invoiceable_type]'
+      #
+      # assert_select 'input[name=?]', 'invoice[account_id]'
+      #
+      # assert_select 'input[name=?]', 'invoice[user_id]'
+      #
+      # assert_select 'input[name=?]', 'invoice[payments]'
+      #
+      # assert_select 'input[name=?]', 'invoice[links]'
 
       assert_select 'input[name=?]', 'invoice[invoice_number]'
 
@@ -48,9 +34,11 @@ RSpec.describe 'invoices/edit', type: :view do
 
       assert_select 'input[name=?]', 'invoice[due_amount]'
 
-      assert_select 'input[name=?]', 'invoice[currency_code]'
+      assert_select 'select[name=?]', 'invoice[currency_code]'
 
-      assert_select 'textarea[name=?]', 'invoice[notes]'
+      assert_select 'trix-editor#invoice_notes'
+
+      assert_select 'input[type="hidden"][name=?]', 'invoice[notes]'
     end
   end
 end
