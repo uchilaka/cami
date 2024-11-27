d5d98b8bcbf74b0af805ee0ccb57331f7b079351
Uche Chilaka <uche@lar.city>
2024-11-26 23:43:09 -0500

    fix: all specs should be passing

--- spec/views/invoices/show.html.tailwindcss_spec.rb
+++ spec/views/invoices/show.html.tailwindcss_spec.rb
@@ -3,36 +3,19 @@
 require 'rails_helper'
 
 RSpec.describe 'invoices/show', type: :view do
+  let(:invoiceable) { Fabricate :account }
+  let(:invoice) { Fabricate(:invoice, invoiceable:, invoice_number: 'INV-04', amount: '9.99', due_amount: '7.99') }
   before(:each) do
-    assign(:invoice, Invoice.create!(
-                       invoiceable_id: '',
-                       invoiceable_type: 'Invoiceable Type',
-                       account: nil,
-                       user: nil,
-                       payments: '',
-                       links: '',
-                       invoice_number: 'Invoice Number',
-                       status: 2,
-                       amount: '9.99',
-                       due_amount: '9.99',
-                       currency_code: 'Currency Code',
-                       notes: 'MyText'
-                     ))
+    assign(:invoice, invoice)
   end
 
   it 'renders attributes in <p>' do
     render
-    expect(rendered).to match(//)
-    expect(rendered).to match(/Invoiceable Type/)
-    expect(rendered).to match(//)
-    expect(rendered).to match(//)
-    expect(rendered).to match(//)
-    expect(rendered).to match(//)
-    expect(rendered).to match(/Invoice Number/)
+    expect(rendered).to match(/Invoice #/)
+    expect(rendered).to match(/INV-04/)
+    expect(rendered).to match(/Invoice #/)
     expect(rendered).to match(/2/)
     expect(rendered).to match(/9.99/)
-    expect(rendered).to match(/9.99/)
-    expect(rendered).to match(/Currency Code/)
-    expect(rendered).to match(/MyText/)
+    expect(rendered).to match(/7.99/)
   end
 end
