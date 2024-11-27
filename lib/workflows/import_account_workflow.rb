61153f982d18ce9cfe579c40c6f04e14aab49267
Uche Chilaka <uche@lar.city>
2024-11-27 05:04:11 -0500

    chore: SAVEPOINT

--- lib/workflows/import_account_workflow.rb
+++ lib/workflows/import_account_workflow.rb
@@ -16,21 +16,17 @@ class ImportAccountWorkflow
       return
     end
 
-    email, display_name, _given_name, _family_name, type =
+    email, display_name, _given_name, _family_name, _type =
       invoice_account.serializable_hash.values_at 'email', 'display_name', 'given_name', 'family_name', 'type'
     display_name = email if display_name.blank?
 
     # Create an account for the business
-    account = Account.create(display_name:, type:)
-    if account.persisted?
-      account.add_role(:customer, invoice.record) if account.is_a?(Business)
-      if email.present?
-        account.add_role(:contact, invoice.record)
-        # Store the email address against the business account
-        # (to help with data reconciliation later on)
-        account.profile.update(email:) if account.is_a?(Business)
-      end
-    else
+    account = Account.create(display_name:, email:)
+    if email.present?
+      # TODO: Add user contact(s) to account metadata
+    end
+
+    if account.errors.any?
       context.errors = account.errors.full_messages
       context.fail!(message: I18n.t('workflows.import_account.errors.generic'))
     end
@@ -46,17 +42,16 @@ class ImportAccountWorkflow
   # Returns a list of any matching account(s) found
   def lookup(params)
     working_params = params.symbolize_keys
-    email, display_name, type = working_params.values_at(:email, :display_name, :type)
+    email, display_name, _type = working_params.values_at(:email, :display_name, :type)
     if email.present?
       # Check for a business account
-      record = Metadata::Business.find_by(email:)&.business
-      return [record] if record.present?
+      records = Account.where(email:)
+      return records if records.any?
 
       # Check for accounts linked to a user via the invoice email address
-      User.find_by(email:)&.accounts || []
+      User.find_by(email:)&.accounts || records
     else
-      filter_params = { display_name:, type: }.reverse_merge(type: 'Business')
-      Account.where(filter_params)
+      Account.where(display_name:)
     end
   end
 end
