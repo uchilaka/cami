25188d81be6fc131ffb17d5585ccf9bf47ba1e0e
Uche Chilaka <uche@lar.city>
2024-11-26 23:31:25 -0500

    fix: (WIP) invoice index spec

--- spec/views/invoices/index.html.tailwindcss_spec.rb
+++ spec/views/invoices/index.html.tailwindcss_spec.rb
@@ -3,53 +3,23 @@
 require 'rails_helper'
 
 RSpec.describe 'invoices/index', type: :view do
+  let(:invoiceable) { Fabricate :account }
   before(:each) do
-    assign(:invoices, [
-             Invoice.create!(
-               invoiceable_id: '',
-               invoiceable_type: 'Invoiceable Type',
-               account: nil,
-               user: nil,
-               payments: '',
-               links: '',
-               invoice_number: 'Invoice Number',
-               status: 2,
-               amount: '9.99',
-               due_amount: '9.99',
-               currency_code: 'Currency Code',
-               notes: 'MyText'
-             ),
-             Invoice.create!(
-               invoiceable_id: '',
-               invoiceable_type: 'Invoiceable Type',
-               account: nil,
-               user: nil,
-               payments: '',
-               links: '',
-               invoice_number: 'Invoice Number',
-               status: 2,
-               amount: '9.99',
-               due_amount: '9.99',
-               currency_code: 'Currency Code',
-               notes: 'MyText'
-             )
-           ])
+    assign(
+      :invoices,
+      [
+        Fabricate(:invoice, invoiceable:, invoice_number: 'INV-01', amount: '9.99', due_amount: '7.99'),
+        Fabricate(:invoice, invoiceable:, invoice_number: 'INV-02', amount: '9.99', due_amount: '7.99'),
+      ]
+    )
   end
 
   it 'renders a list of invoices' do
     render
     cell_selector = 'div>p'
-    assert_select cell_selector, text: Regexp.new(''.to_s), count: 2
-    assert_select cell_selector, text: Regexp.new('Invoiceable Type'.to_s), count: 2
-    assert_select cell_selector, text: Regexp.new(nil.to_s), count: 2
-    assert_select cell_selector, text: Regexp.new(nil.to_s), count: 2
-    assert_select cell_selector, text: Regexp.new(''.to_s), count: 2
-    assert_select cell_selector, text: Regexp.new(''.to_s), count: 2
-    assert_select cell_selector, text: Regexp.new('Invoice Number'.to_s), count: 2
-    assert_select cell_selector, text: Regexp.new(2.to_s), count: 2
+    assert_select cell_selector, text: Regexp.new('Invoice #'.to_s), count: 2
+    assert_select cell_selector, text: Regexp.new('INV-'), count: 2
     assert_select cell_selector, text: Regexp.new('9.99'.to_s), count: 2
-    assert_select cell_selector, text: Regexp.new('9.99'.to_s), count: 2
-    assert_select cell_selector, text: Regexp.new('Currency Code'.to_s), count: 2
-    assert_select cell_selector, text: Regexp.new('MyText'.to_s), count: 2
+    assert_select cell_selector, text: Regexp.new('7.99'.to_s), count: 2
   end
 end
