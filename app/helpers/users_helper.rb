fd3f75f25089035af5148f2bb837e124775f5d49
Uche Chilaka <uche@lar.city>
2024-12-18 09:43:36 -0500

    refactor: load profile image url (if present)

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,8 +4,12 @@ module UsersHelper
   def avatar_url(_user)
     # TODO: Create a metadata (aliased as profile) field in the user model
     #   and store the image_url there
-    # return user.profile.image_url if user_signed_in?
+    return profile_image_url if user_signed_in? && profile_image_url.present?
 
     image_url 'person-default.svg'
+  end
+
+  def profile_image_url
+    @profile_image_url ||= current_user.profile['image_url']
   end
 end
