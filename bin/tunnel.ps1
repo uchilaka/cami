87946939f4c399d17e0b0bb40c4926621bf6c7ac
Uche Chilaka <uche@lar.city>
2024-12-22 04:57:31 -0500

    SAVEPOINT

---
+++ bin/tunnel.ps1
@@ -0,0 +1,7 @@
+# Accomplish something that looks like this:
+# ngrok start --all --config=config/ngrok.yml
+#
+# Issues to resolve:
+# 1. Resolve the path of the ngrok config file
+# 2. Resolve the issues with the ExecutionPolicy to allow running the script. Determine the right one
+#    and update the README.md to instruct devs to set it up before attempting to start the tunnel.
