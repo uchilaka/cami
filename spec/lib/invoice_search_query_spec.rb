e8c387824f062ddf1d759ae60a320a5ce175e590
Uche Chilaka <uche@lar.city>
2025-01-26 08:54:36 -0500

    refactor: update invoice_search_query_spec

--- spec/lib/invoice_search_query_spec.rb
+++ spec/lib/invoice_search_query_spec.rb
@@ -12,11 +12,12 @@ RSpec.describe InvoiceSearchQuery do
     described_class.new(qs, params: filtered_params)
   end
 
-  context 'filtering by status and sort by descending due date (array of hashes inputs)' do
+  shared_examples 'search filtering by status and sorting by due date (with array of hashes inputs)' do |qs, status, expected_predicates|
     let(:params) do
       {
         'mode' => 'array',
-        'f' => [{ 'field' => 'status', 'value' => 'PAID' }],
+        'q' => qs,
+        'f' => [{ 'field' => 'status', 'value' => status.upcase }],
         's' => [{ 'field' => 'dueAt', 'direction' => 'desc' }]
       }
     end
@@ -27,19 +28,48 @@ RSpec.describe InvoiceSearchQuery do
       described_class.new(qs, params: filtered_params)
     end
 
-    it { expect(subject.predicates).to match(status_eq: 'paid') }
+    it { expect(subject.predicates).to match(expected_predicates) }
     it { expect(subject.sorters).to include('due_at desc') }
   end
 
-  context 'filtering by status and sort by descending due date' do
+  shared_examples 'search filtering by status and sorting by due date' do |qs, status, expected_predicates|
     let(:params) do
       {
-        'f' => { 'status' => 'PAID' },
+        'q' => qs,
+        'f' => { 'status' => status.upcase },
         's' => { 'dueAt' => 'desc' }
       }
     end
 
-    it { expect(subject.predicates).to match(status_eq: 'paid') }
+    subject do
+      filtered_params =
+        ActionController::Parameters.new(params).permit(:q, :mode, f: {}, s: {})
+      described_class.new(qs, params: filtered_params)
+    end
+
+    it { expect(subject.predicates).to match(expected_predicates) }
     it { expect(subject.sorters).to include('due_at desc') }
+  end
+
+  context 'without query string' do
+    it_should_behave_like 'search filtering by status and sorting by due date (with array of hashes inputs)',
+                          nil, 'PAID', { status_eq: 'paid' }
+    it_should_behave_like 'search filtering by status and sorting by due date',
+                          nil, 'SENT', { status_eq: 'sent' }
+  end
+
+  context 'with query string' do
+    it_should_behave_like 'search filtering by status and sorting by due date (with array of hashes inputs)',
+                          'logistics', 'PAID',
+                          {
+                            'invoiceable_of_Account_type_display_name_or_invoiceable_of_Account_type_email_cont' => 'logistics',
+                            :status_eq => 'paid'
+                          }
+    it_should_behave_like 'search filtering by status and sorting by due date',
+                          'logistics', 'SENT',
+                          {
+                            'invoiceable_of_Account_type_display_name_or_invoiceable_of_Account_type_email_cont' => 'logistics',
+                            :status_eq => 'sent'
+                          }
   end
 end
