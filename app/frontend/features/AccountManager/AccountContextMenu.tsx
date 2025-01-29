d8a7fe29b873e091dadf2f170d3f387da759e0d7
Uche Chilaka <uche@lar.city>
2025-01-29 04:46:02 -0500

    refactor: (WIP) layout & styling updates

--- app/frontend/features/AccountManager/AccountContextMenu.tsx
+++ app/frontend/features/AccountManager/AccountContextMenu.tsx
@@ -6,20 +6,58 @@ const AccountContextMenu = () => {
   return (
     <div className="max-w-screen-xl px-4 py-3 mx-auto">
       <div className="flex items-center">
-        <ul className="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
-          {/* IF: 1+ accounts are selected */}
-          <li>
-            <a href="#" className="text-gray-900 dark:text-white hover:underline" aria-current="page">
-              Combine
-            </a>
-          </li>
-          {/* IF: 1+ accounts are selected */}
-          <li>
-            <a href="#" className="text-gray-900 dark:text-white hover:underline">
-              Archive
-            </a>
-          </li>
-        </ul>
+        <div className="inline-flex rounded-md shadow-xs" role="group">
+          <button
+            type="button"
+            className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-s-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white"
+          >
+            <svg
+              className="w-6 h-6 text-gray-800 dark:text-white"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeWidth="2"
+                d="M4.5 17H4a1 1 0 0 1-1-1 3 3 0 0 1 3-3h1m0-3.05A2.5 2.5 0 1 1 9 5.5M19.5 17h.5a1 1 0 0 0 1-1 3 3 0 0 0-3-3h-1m0-3.05a2.5 2.5 0 1 0-2-4.45m.5 13.5h-7a1 1 0 0 1-1-1 3 3 0 0 1 3-3h3a3 3 0 0 1 3 3 1 1 0 0 1-1 1Zm-1-9.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Z"
+              />
+            </svg>
+            Combine Accounts
+          </button>
+          {/* <button
+              type="button"
+              className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white"
+            >
+              Middle Item
+            </button> */}
+          <button
+            type="button"
+            className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-e-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white"
+          >
+            <svg
+              className="w-6 h-6 text-gray-800 dark:text-white"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M10 12v1h4v-1m4 7H6a1 1 0 0 1-1-1V9h14v9a1 1 0 0 1-1 1ZM4 5h16a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1Z"
+              />
+            </svg>
+            Archive
+          </button>
+        </div>{' '}
       </div>
     </div>
   )
--- app/frontend/features/InvoiceManager/InvoiceContextMenu.tsx
+++ app/frontend/features/InvoiceManager/InvoiceContextMenu.tsx
@@ -6,20 +6,28 @@ const InvoiceContextMenu = () => {
   return (
     <div className="max-w-screen-xl px-4 py-3 mx-auto">
       <div className="flex items-center">
-        <ul className="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
-          {/* IF: 1+ invoices are selected */}
-          <li>
-            <a href="#" className="text-gray-900 dark:text-white hover:underline" aria-current="page">
+        <div className="inline-flex rounded-md shadow-xs" role="group">
+          <div className="inline-flex rounded-md shadow-xs" role="group">
+            <button
+              type="button"
+              className="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-s-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white"
+            >
               Link Account
-            </a>
-          </li>
-          {/* IF: 1+ invoices are selected */}
-          <li>
-            <a href="#" className="text-gray-900 dark:text-white hover:underline">
+            </button>
+            {/* <button
+              type="button"
+              className="px-4 py-2 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white"
+            >
+              Middle Item
+            </button> */}
+            <button
+              type="button"
+              className="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-e-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white"
+            >
               Archive
-            </a>
-          </li>
-        </ul>
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   )
