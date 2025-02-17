cdd5085e982574ac5336bb927c567f7206effe46
Uche Chilaka <uche@lar.city>
2025-02-16 19:13:18 -0500

    refactor: pull forward some security fixes from #222

--- lib/zoho/api/account.rb
+++ lib/zoho/api/account.rb
@@ -3,20 +3,38 @@
 module Zoho
   module API
     class Account < Model
-      @@resource_url ||= nil
+      # About class (instance) variables: https://www.ruby-lang.org/en/documentation/faq/8/
+      @resource_url ||= nil
 
       class << self
-        def resource_url
-          @@resource_url ||=
-            unless @@resource_url.present?
-              response = connection.get('/oauth/serverinfo')
-              data = response.body || {}
-              @@resource_url = data.dig('locations', 'us')
-            end
+        def upsert(record)
+          access_token = AccessToken.generate['access_token']
+          resource_uri = "/crm/v7/#{module_name}/upsert"
+          payload = {
+            data: [
+              Zoho::AccountSerializer.new(record).serializable_hash
+            ],
+            duplicate_check_fields: %w[Email Phone],
+            trigger: ['workflow']
+          }
+          response = connection(access_token:).post(resource_uri, payload)
+          response.body
         end
 
-        def base_url
-          'https://accounts.zoho.com'
+        def resource_url(auth: true)
+          return auth_endpoint_url if auth
+
+          base_url(auth:)
+        end
+
+        def base_url(auth: false)
+          return 'https://accounts.zoho.com' if auth
+
+          super
+        end
+
+        def module_name
+          name.to_s.split('::').last.pluralize.capitalize
         end
       end
     end
--- lib/zoho/api/model.rb
+++ lib/zoho/api/model.rb
@@ -3,37 +3,80 @@
 module Zoho
   module API
     class Model
-      @@connection ||= nil
-
       class << self
-        def resource_url
-          raise NotImplementedError
+        def connection(auth: false, access_token: nil)
+          url = base_url(auth:)
+          Faraday.new(url:) do |builder|
+            builder.request :authorization, 'Zoho-oauthtoken', access_token if access_token.present?
+            builder.request :json
+            builder.response :json
+            builder.response :logger if Rails.env.development?
+            builder.response :raise_error, include_request: true
+          end
         end
 
-        def connection(_auth: false)
-          @@connection ||=
-            unless @@connection.present?
-              Faraday.new(
-                url: base_url,
-                headers: {
-                  Accept: 'application/json'
-                }
-              ) do |builder|
-                # if _auth
-                #   builder.request :authorization, :basic,
-                #                   Zoho::Credentials.client_id,
-                #                   Zoho::Credentials.client_secret
-                # end
-                # builder.request :json
-                builder.response :json
-                builder.response :logger if Rails.env.development?
-                builder.response :raise_error, include_request: true
-              end
-            end
-        end
-
-        def base_url
+        def base_url(*)
           'https://www.zohoapis.com'
+        end
+
+        def auth_endpoint_url
+          @auth_endpoint_url ||= regional_oauth_endpoint_url
+        end
+
+        def regional_oauth_endpoint_url(region = 'us')
+          unless allowed_regions.include?(region.to_sym)
+            exception = ::LarCity::Errors::Unknown3rdPartyHostError.new('Unsupported 3rd party service region')
+            raise exception
+          end
+
+          response = connection(auth: true).get('/oauth/serverinfo')
+          data = response.body || {}
+          url = data.dig('locations', region.to_s)
+          if valid_http_host?(url)
+            url
+          else
+            raise ::LarCity::Errors::Unknown3rdPartyHostError unless valid_http_host?(url)
+          end
+        end
+
+        def fields_list_url
+          "https://crm.zoho.com/crm/org#{org_id}/settings/api/modules/#{module_name}?step=FieldsList"
+        end
+
+        def resource_url(args = {})
+          raise NotImplementedError, "You must implement the #{name}.#{__method__} method"
+        end
+
+        protected
+
+        def valid_http_host?(url_or_hostname)
+          hostname = URI.parse(url_or_hostname).host
+          allowed_hosts.include?(hostname)
+        end
+
+        private
+
+        def allowed_regions
+          @allowed_regions ||=
+            AppUtils
+              .allowed_hosts_for(provider: :zoho)[:by_region]
+              .keys
+        end
+
+        def allowed_hosts
+          @allowed_hosts ||=
+            AppUtils
+              .allowed_hosts_for(provider: :zoho)[:by_region]
+              .entries
+              .map { |_r, host| host }
+        end
+
+        def module_name
+          raise NotImplementedError, "You must implement the #{name}.#{__method__} method"
+        end
+
+        def org_id
+          ENV.fetch('CRM_ORG_ID', Rails.application.credentials&.zoho&.org_id)
         end
       end
     end
