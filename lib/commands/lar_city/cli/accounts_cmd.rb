a6435b7bfeeef2913aded0ed2f8197b575436c8a
Uche Chilaka <uche@lar.city>
2025-02-18 04:56:34 -0500

    chore: SAVEPOINT

--- lib/commands/lar_city/cli/accounts_cmd.rb
+++ lib/commands/lar_city/cli/accounts_cmd.rb
@@ -13,25 +13,36 @@ module LarCity
              aliases: %w[--qs -q]
       desc 'dedupe', 'Consolidate duplicate accounts'
       def dedupe
+        prompt_for_query_string_search if qs.blank?
         filtered_params =
           ActionController::Parameters
-            .new(options)
+            .new(options.merge(s: { updatedAt: 'desc' }))
             .permit(:q, f: {}, s: {})
         if verbose?
           input = { qs:, filtered_params: }
           ap input
         end
         @search_query = AccountSearchQuery.new(qs, params: filtered_params)
-        ap @search_query.predicates if verbose?
+        if verbose?
+          output = {
+            predicates: @search_query.predicates,
+            sorters: @search_query.sorters
+          }
+          ap output
+        end
         @query = Account.ransack(@search_query.predicates)
         @query.sorts = @search_query.sorters if @search_query.sorters.any?
         accounts = @query.result(distinct: true)
-        say "Found #{accounts.count} #{things(accounts.count, name: 'account')} that might be duplicates", :green
+        say "Found #{accounts.count} #{things(accounts.count, name: 'account')} that might be duplicates", :yellow
       end
 
       no_commands do
         def qs
-          options[:query_string]
+          @qs ||= options[:query_string]
+        end
+
+        def prompt_for_query_string_search
+          @qs = ask('Enter a query string to search for accounts: ').chomp
         end
       end
     end
