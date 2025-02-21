9ac9ed45630eed9b1c797e18ee5bc3fdbd41c3ba
Uche Chilaka <uche@lar.city>
2025-02-21 04:45:55 -0500

    chore: SAVEPOINT

---
+++ db/data/20250221093342_add_knapsack_pro_to_vendors.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'awesome_print'
+
+# 20250221093342
+class AddKnapsackProToVendors < ActiveRecord::Migration[7.2]
+  def up
+    knapsack_pro = Vendor.new(
+      slug: 'knapsack-pro',
+      display_name: 'Knapsack Pro',
+      metadata: {
+        website_url: 'https://knapsackpro.com',
+        support_url: 'https://knapsackpro.com/contact',
+        resources: {
+          cami: {
+            name: 'CAMI Project @ Knapsack Pro',
+            url: 'https://knapsackpro.com/dashboard/organizations/4019/projects/2608'
+          }
+        }
+      }
+    )
+    knapsack_pro.save
+    if knapsack_pro.persisted?
+      puts "🪄 Created vendor: #{knapsack_pro.display_name} [#{knapsack_pro.slug}]"
+    else
+      puts "❌ Failed to create vendor: #{knapsack_pro.display_name} [#{knapsack_pro.slug}]"
+      ap knapsack_pro.errors.full_messages if knapsack_pro.errors.any?
+    end
+  end
+
+  def down
+    Vendor.find_by(slug: 'knapsack-pro')&.destroy
+  end
+end
