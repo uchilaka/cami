c9ff62435194321cdfb9e71195ebe45d8db3810d
Uche Chilaka <uche@lar.city>
2025-04-02 09:10:14 -0400

    refactor: have upsert logic implemented in Zoho::API::Account

--- lib/zoho/api/account.rb
+++ lib/zoho/api/account.rb
@@ -20,10 +20,33 @@ module Zoho
           if pretend
             Rails.logger.info("Pretending to upsert #{module_name} record data", payload:)
             return
+          else
+            Rails.logger.info('Upserting Zoho account record', id: record.id)
           end
-
           response = connection(access_token:).post(resource_uri, payload)
-          response.body
+          result = response.body
+          info = result.dig('data', 0)
+          code, action = info&.values_at('code', 'action')
+          if code == 'SUCCESS'
+            remote_crm_id = info.dig('details', 'id')
+            case action
+            when 'insert'
+              record.update(remote_crm_id:)
+            else
+              Rails.logger.warn(
+                'An unsupported action occurred against a Zoho account record',
+                record: record.serializable_hash,
+                result:
+              )
+            end
+          else
+            Rails.logger.error(
+              'Failed to upsert Zoho account record',
+              record: record.serializable_hash,
+              result:
+            )
+          end
+          result
         end
 
         def resource_url(auth: true)
